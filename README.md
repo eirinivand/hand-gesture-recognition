3de0447b925e52a1dd196a3dc8f99efc5ffba1e4
Eirini Vandorou <eirinivand@gmail.com>
2020-05-18 05:01:21 +0300

    deleted camera.py -left overs

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 kaggle datasets download changethetuneman/openpose-model -f pose_iter_102000.caffemodel -p files/hand
 
-kaggle datasets download gti-upm/leapgestrecog 
+kaggle datasets download gti-upm/leapgestrecog -p files
--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, redirect, url_for, render_template, Response
-from camera import VideoCamera
 
 app = Flask(__name__)
 
@@ -25,13 +24,6 @@ def gen(camera):
         frame = camera.get_frame()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-
-
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen(VideoCamera()),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
-
 
 @app.route("/camera-feed", methods=['POST', 'GET'])
 def camera_feed():
